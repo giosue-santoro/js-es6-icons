461a8464d59b0d027a0a859c4d969b29d374ef80
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-21 15:58:12 +0100

    array

---
+++ js/script.js
@@ -0,0 +1,117 @@
+//ARRAY DI ICONE
+const icons = [
+{
+    nome: 'cat',
+    prefisso: 'fa-', 
+    tipo: 'animal',
+    famiglia: 'fas'
+},
+{
+    nome: 'crow',
+    prefisso: 'fa-', 
+    tipo: 'animal',
+    famiglia: 'fas'
+},
+{
+    nome: 'dove',
+    prefisso: 'fa-', 
+    tipo: 'animal',
+    famiglia: 'fas'
+},
+{
+    nome: 'dog',
+    prefisso: 'fa-', 
+    tipo: 'animal',
+    famiglia: 'fas'
+},
+{
+    nome: 'dragon',
+    prefisso: 'fa-', 
+    tipo: 'animal',
+    famiglia: 'fas'
+},
+{
+    nome: 'horse',
+    prefisso: 'fa-', 
+    tipo: 'animal',
+    famiglia: 'fas'
+},
+{
+    nome: 'hippo',
+    prefisso: 'fa-', 
+    tipo: 'animal',
+    famiglia: 'fas'
+},
+{
+    nome: 'fish',
+    prefisso: 'fa-', 
+    tipo: 'animal',
+    famiglia: 'fas'
+},
+{
+    nome: 'carrot',
+    prefisso: 'fa-', 
+    tipo: 'fruit',
+    famiglia: 'fas'
+},
+{
+    nome: 'apple-alt',
+    prefisso: 'fa-', 
+    tipo: 'fruit',
+    famiglia: 'fas'
+},
+{
+    nome: 'lemon',
+    prefisso: 'fa-', 
+    tipo: 'fruit',
+    famiglia: 'fas'
+},
+{
+    nome: 'pepper-hot',
+    prefisso: 'fa-', 
+    tipo: 'fruit',
+    famiglia: 'fas'
+},
+{
+    nome: 'user-astronaut',
+    prefisso: 'fa-', 
+    tipo: 'person',
+    famiglia: 'fas'
+},
+{
+    nome: 'user-graduate',
+    prefisso: 'fa-', 
+    tipo: 'person',
+    famiglia: 'fas'
+},
+{
+    nome: 'user-ninja',
+    prefisso: 'fa-', 
+    tipo: 'person',
+    famiglia: 'fas'
+},
+{
+    nome: 'user-secret',
+    prefisso: 'fa-', 
+    tipo: 'person',
+    famiglia: 'fas'
+}
+]
+
+//forEach per visualizzare le icone con proprio nome
+const iconsContainer = document.getElementById('container');
+
+icons.forEach((element) => {
+    iconsContainer.innerHTML =+ `
+        
+    `
+});
+
+
+
+
+
+
+
+
+
